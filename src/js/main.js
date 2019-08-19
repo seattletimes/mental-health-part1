4312448fac1661813008a4905115ec1f86d913c2
emilymeng <eeng@seattletimes.com>
2019-08-19 13:18:00 -0700

    hyperlinks

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,14 @@
 
 require("component-responsive-frame/child");
 require("./bc-preroll");
+
+// var promise = document.querySelector('video').play();
+
+// if (promise !== undefined) {
+//   promise.then(_ => {
+//     // Autoplay started!
+//   }).catch(error => {
+//     // Autoplay was prevented.
+//     // Show a "Play" button so that user can start playback.
+//   });
+// };
